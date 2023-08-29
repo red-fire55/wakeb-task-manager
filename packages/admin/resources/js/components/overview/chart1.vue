e40478ba4a2f45a0028c88ac4cc8164d74a9550d
mahmod salah <redfire9413@gmail.com>
2023-08-29 03:07:02 +0300

    finished overview & projects table

--- packages/admin/resources/js/components/overview/chart1.vue
+++ packages/admin/resources/js/components/overview/chart1.vue
@@ -1,31 +1,27 @@
 <template>
-    <section class="">
-        <div class="">
-            <Card class="py-4 ltr:pl-4 ltr:pr-3 rtl:pr-4 rtl:pl-3">
-                <h3 class="mb-4 font-medium">
-                    {{ __('chart title') }}
-                </h3>
+  <section class="">
+    <div class="">
+      <Card class="py-4 ltr:pl-4 ltr:pr-3 rtl:pr-4 rtl:pl-3">
+        <h3 class="mb-4 font-medium">Tasks & Milestones</h3>
 
-                <div style="">
-                    <LineChart
-                        id="tasks-chart"
-                        :data="home.charts?.chart_tasks_weekly"
-                        tooltip-label="Tasks"
-                        background-color="rgba(79, 70, 229, 0.2)"
-                        border-color="#4F46E5"
-                    />
-                </div>
-            </Card>
+        <div style="">
+          <LineChart
+            id="chart-tasks-for-milestone"
+            :data="home.charts?.chart_tasks_for_milestone"
+            tooltip-label="Tasks"
+            background-color="rgba(79, 70, 229, 0.2)"
+            border-color="#4F46E5"
+          />
         </div>
-
-    </section>
+      </Card>
+    </div>
+  </section>
 </template>
 
 <script setup lang="ts">
-import { useHomeStore } from 'Store/home'
-import { LineChart } from 'thetheme'
-import { Card } from 'thetheme'
+import { useHomeStore } from "Store/home";
+import { LineChart } from "thetheme";
+import { Card } from "thetheme";
 
-const home = useHomeStore()
+const home = useHomeStore();
 </script>
-
--- packages/admin/resources/js/views/projects/TabOverview.vue
+++ packages/admin/resources/js/views/projects/TabOverview.vue
@@ -1,41 +1,53 @@
 <template>
-    <div v-if="processing" class="mt-8 flex justify-center">
-        <Loader size="40" color="#5850ec" />
+  <div v-if="processing" class="mt-8 flex justify-center">
+    <Loader size="40" color="#5850ec" />
+  </div>
+  <div v-else>
+    <div class="flex gap-[4%] mt-10">
+      <div class="w-[48%] bg-white p-4 rounded-lg shadow-lg">
+        <Progress class="w-[96%]" :data="statistics.project_progress" />
+        <taskCards
+          class="w-[96%]"
+          :data="{
+            opened_tasks: statistics.opened_tasks,
+            completed_tasks: statistics.completed_tasks,
+          }"
+        />
+        <milestoneCards
+          class="w-[96%]"
+          :data="{
+            opened_milestones: statistics.opened_milestones,
+            completed_milestones: statistics.completed_milestones,
+          }"
+        />
+      </div>
+      <div class="w-[48%]">
+        <chart1 :data="statistics.project_progress" />
+        <chart2 :data="statistics.project_progress" />
+      </div>
     </div>
-    <div v-else>
-        <div class="flex  gap-[4%] mt-10 ">
-            <div class="w-[48%] bg-white p-4 rounded-lg shadow-lg ">
-                <Progress class="w-[96%]"/>
-                <taskCards class="w-[96%]"/>
-                <milestoneCards class="w-[96%]"/>
-            </div>
-            <div class="w-[48%] ">
-                <chart1 />
-                <chart2 />
-            </div>
-        </div>
-        
-    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-    import Progress from "Component/overview/progress.vue";
-    import taskCards from "Component/overview/task-cards.vue";
-    import milestoneCards from "Component/overview/milestone-cards.vue";
-    import chart1 from "Component/overview/chart1.vue";
-    import chart2 from "Component/overview/chart2.vue";
+import Progress from "Component/overview/progress.vue";
+import taskCards from "Component/overview/task-cards.vue";
+import milestoneCards from "Component/overview/milestone-cards.vue";
+import chart1 from "Component/overview/chart1.vue";
+import chart2 from "Component/overview/chart2.vue";
+import { axios } from "spack";
+import { useProjectDetail } from "Store/project-detail";
 
-    import {
-        Loader
-    } from "@/thetheme";
-    import {
-        useIndexStore
-    } from "@/spack";
-    import {
-        ref
-    } from 'vue'
-    
+const project = useProjectDetail();
+let statistics = ref({});
+
+axios.get(`projects/${project.data.id}/statistics`).then((res) => {
+  statistics.value = res.data;
+});
+
+import { Loader } from "@/thetheme";
+import { useIndexStore } from "@/spack";
+import { ref } from "vue";
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
