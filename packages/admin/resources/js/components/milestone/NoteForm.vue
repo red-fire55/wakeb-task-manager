741ed8e11d81adc50df62526673932fb814ab30b
Mahmod Salah <M.Salah@wakeb.tech>
2023-09-14 09:19:08 +0300

    applied notes history

--- packages/admin/resources/js/components/milestone/NoteForm.vue
+++ packages/admin/resources/js/components/milestone/NoteForm.vue
@@ -1,6 +1,6 @@
 <template>
-  <FormModal :id="id" :name="name" uri="milestone">
-    <!-- <ul
+  <FormModal :id="props.id" :name="name" uri="milestone">
+    <ul
       class="hidden text-sm font-medium text-center text-gray-500 divide-x divide-gray-200 rounded-lg shadow sm:flex dark:divide-gray-700 dark:text-gray-400 col-span-12 mt-2 mt-1"
     >
       <li class="w-full" @click="show = 'create'">
@@ -29,16 +29,44 @@
           >Show Previous Notes</a
         >
       </li>
-    </ul> -->
+    </ul>
     <TextArea
       name="note"
       label="Notes"
       class="col-span-12 mt-2 mt-1"
       v-if="show == 'create'"
     />
-    <!-- <div class="col-span-12 mt-2 mt-1" v-else>
-      <div class="w-100 p-3" v-for="(item, i) in notes" :key="i"></div>
-    </div> -->
+    <div class="col-span-12 mt-2 mt-1" v-else>
+      <ol class="relative border-l border-gray-200 mt-10">
+        <li class="mb-10 ml-6" v-for="(note, i) in notes" :key="i">
+          <span
+            class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white"
+          >
+            <img
+              class="rounded-full shadow-lg"
+              :src="note.causer.avatar"
+              alt="Bonnie image"
+            />
+          </span>
+          <div
+            class="items-center justify-between p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:flex"
+          >
+            <time class="mb-1 text-xs font-normal text-gray-400 sm:order-last sm:mb-0">{{
+              note.updated_at.toString().substr(0, 10)
+            }}</time>
+            <div class="text-sm font-normal text-gray-500 dark:text-gray-300">
+              قام {{ note?.causer?.name }}
+              بإدخال الملحوظة
+              <a
+                href="#"
+                class="font-semibold text-blue-600 dark:text-blue-500 hover:underline"
+                >{{ note?.properties?.new.note }}</a
+              >
+            </div>
+          </div>
+        </li>
+      </ol>
+    </div>
   </FormModal>
 </template>
 
@@ -48,7 +76,7 @@ import { useFormStore, useIndexStore, useModalsStore, axios } from "spack";
 import { useProjectDetail } from "Store/project-detail";
 import { ref } from "vue";
 
-defineProps<{
+const props = defineProps<{
   id?: number;
 }>();
 
@@ -59,7 +87,9 @@ const TextArea = useTextArea<any>();
 const project = useProjectDetail();
 let show = ref("create");
 let notes = ref([]);
-axios.get(`logs?milestone=3`).then((res) => {
+let milestone_id = props.id
+axios.get(`logs?milestone=${milestone_id}`).then((res) => {
+  localStorage.setItem("note", JSON.stringify(res.data.data))
   notes.value = res.data.data;
 });
 setTimeout(() => {
